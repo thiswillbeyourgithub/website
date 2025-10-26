fb40fe42a1113544e47d1f48ef76664ca7f8e8ba
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-10-26 12:12:32 +0100

    fix: invert day and night button toggle icon
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -32,10 +32,10 @@ let setTheme = (theme) => {
 
   if (use_theme === "dark") {
     $("html").attr("data-theme", "dark");
-    $("#theme-icon").removeClass("fa-sun").addClass("fa-moon");
+    $("#theme-icon").removeClass("fa-moon").addClass("fa-sun");
   } else if (use_theme === "light") {
     $("html").removeAttr("data-theme");
-    $("#theme-icon").removeClass("fa-moon").addClass("fa-sun");
+    $("#theme-icon").removeClass("fa-sun").addClass("fa-moon");
   }
 };
 
